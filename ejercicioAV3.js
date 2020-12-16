322bca5485ea8e970e6eb354ab7994477e88fa59
mario <mrebollogarcia89@gmail.com>
2020-12-16 14:14:03 +0100

    Hola

--- ejercicioAV3.js
+++ ejercicioAV3.js
@@ -9,17 +9,27 @@ window.onload = () => {
                 }
             })
             .then(datos => {
-                let usuarios=[];
-                let contraseña=[];
+                let usuarios = [];
+                let contraseñas = [];
                 datos.forEach(element => {
                     usuarios.push(element.usuario);
-                    contraseña.push(element.contraseña)
+                    contraseñas.push(element.contraseña)
                 });
-                if(usuarios.includes(document.getElementById("usuario"))){
+                console.log(usuarios);
+                console.log(contraseñas);
+                
+                if (usuarios.includes(document.getElementById("usuario"))) {
                     console.log("usuario existe");
-                    
+                    let indiceUsuario = usuarios.indexOf(document.getElementById("usuario"));
+                    if (contraseña[indiceUsuario] == document.getElementById("contraseña")) {
+                        alert("Esta to correcto");
+                    } else {
+                        alert("Contraseña Invalida");
+                    }
+                } else {
+                    alert("Usuario no existe");
                 }
             })
-           
+
     }, false)
 }
